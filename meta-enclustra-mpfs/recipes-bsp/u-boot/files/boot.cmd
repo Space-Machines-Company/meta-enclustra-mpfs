bf3882dfb3a1d150adb3af169dfbb72da61fb853
Andreas Buerkler <andreas.buerkler@enclustra.com>
2022-10-21 20:39:57 +0200

    Remove tftp boot mode / cleanup board files

--- meta-enclustra-mpfs/recipes-bsp/u-boot/files/boot.cmd
+++ meta-enclustra-mpfs/recipes-bsp/u-boot/files/boot.cmd
@@ -1,4 +1,4 @@
 # Try to boot a fitImage from the MMC
-if load ${devtype} ${devnum}:${distro_bootpart} ${ramdisk_addr_r} fitImage; then
-    bootm ${ramdisk_addr_r}
+if load ${devtype} ${devnum}:${distro_bootpart} ${fitimage_addr_r} fitImage; then
+    bootm ${fitimage_addr_r}
 fi;
